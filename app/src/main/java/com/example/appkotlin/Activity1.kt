e12a971c46e63ad530bf51988fe866ffeb6d6c22
Oscar <oscarwruedakollmer@gmail.com>
2024-02-22 17:59:20 +0100

    He implementado la logica de los botones, uno cambia el fondo al color blanco y el otro cambia al activity2, pero sin cerrar el activity1, de tal forma que puede regresar

--- app/src/main/java/com/example/appkotlin/Activity1.kt
+++ app/src/main/java/com/example/appkotlin/Activity1.kt
@@ -1,21 +1,35 @@
 package com.example.appkotlin
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class Activity1 : AppCompatActivity() {
+class Activity1 : AppCompatActivity(), OnClickListener {
 
     lateinit var rootLayout: ConstraintLayout
     lateinit var secondLayout: ConstraintLayout
 
+    lateinit var btnCambiarFondo:Button
+    lateinit var btnCambiarActivity:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
         rootLayout = this.findViewById(R.id.rootLayout)
         secondLayout = this.findViewById(R.id.secondLayout)
+
+        btnCambiarActivity = this.findViewById(R.id.cambiarActivity)
+        btnCambiarFondo = this.findViewById(R.id.cambiarFondo)
+
+        btnCambiarActivity.setOnClickListener(this)
+        btnCambiarFondo.setOnClickListener(this)
         main()
     }
 
@@ -24,17 +38,16 @@ class Activity1 : AppCompatActivity() {
     }
 
     private fun body(rootColor: Int, secondColor: Int) {
-        // Establecer el color de fondo del "cuerpo"
         rootLayout.setBackgroundColor(rootColor)
         secondLayout.setBackgroundColor(secondColor)
     }
-    override fun onPause() {
-        super.onPause()
-        Log.v("MainActivity", "Esta en pausa¡")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.v("MainActivity", "Reanuda la aplicacion")
+    override fun onClick(v: View?) {
+        if (v!!.id==R.id.cambiarFondo){
+            secondLayout.setBackgroundColor(Color.WHITE)
+        } else if (v!!.id==R.id.cambiarActivity){
+            var activity2:Intent = Intent(this, Activity2::class.java)
+            startActivity(activity2)
+        }
     }
 }
