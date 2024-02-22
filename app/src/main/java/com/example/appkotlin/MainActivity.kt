5b88cb9ca58cb4ab1dcfb7c53073bcbeac436cc8
Oscar <oscarwruedakollmer@gmail.com>
2024-02-22 16:45:20 +0100

    He creado un constraintLayout(rootLayout) el cual es de color negro y dentro he creado otro constraintLayout(secondLayout) el cual es de color rojo y tiene un margen top y bottom de 8 px

---
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.appkotlin
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var rootLayout: ConstraintLayout
+    lateinit var secondLayout: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        rootLayout = this.findViewById(R.id.rootLayout)
+        secondLayout = this.findViewById(R.id.secondLayout)
+        main()
+    }
+
+    private fun main() {
+        body(Color.BLACK, Color.RED)
+    }
+
+    private fun body(rootColor: Int, secondColor: Int) {
+        // Establecer el color de fondo del "cuerpo"
+        rootLayout.setBackgroundColor(rootColor)
+        secondLayout.setBackgroundColor(secondColor)
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.v("MainActivity", "Esta en pausa¡")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("MainActivity", "Reanuda la aplicacion")
+    }
+}
