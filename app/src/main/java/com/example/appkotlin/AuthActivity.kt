e769c1c4bb3327c02ce957c3feeecf0682317d61
Oscar <oscarwruedakollmer@gmail.com>
2024-02-23 17:33:16 +0100

    Añadido el nav_graph

---
+++ app/src/main/java/com/example/appkotlin/AuthActivity.kt
@@ -0,0 +1,17 @@
+package com.example.appkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        setContentView(R.layout.activity_auth)
+    }
+}
