990ecc5ed11f156f3f4c47a5e80ad7ba985bc857
Oscar <oscarwruedakollmer@gmail.com>
2024-02-24 20:52:05 +0100

    se ha implementado dos cajas de texto para recoger un correo y una contraseña y poder iniciar sesion en el LoginFragment

--- app/src/main/java/com/example/appkotlin/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/appkotlin/fragments/LoginFragment.kt
@@ -1,60 +1,79 @@
 package com.example.appkotlin.fragments
-
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.appkotlin.Activity1
 import com.example.appkotlin.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : Fragment(), OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var edtEmail: EditText
+    lateinit var edtPassword: EditText
+    lateinit var btnLogin: Button
+    lateinit var btnRegister: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        auth = Firebase.auth
+        navController = findNavController()
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        edtEmail = view.findViewById(R.id.edtEmail)
+        edtPassword = view.findViewById(R.id.edtPassword)
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnRegister = view.findViewById(R.id.btnRegister)
+        btnLogin.setOnClickListener(this)
+        btnRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnLogin -> {
+                val sEmail = edtEmail.text.toString()
+                val sPassword = edtPassword.text.toString()
+
+                if (checkFields()) {
+                    Snackbar.make(requireView(), "Por favor, completa todos los campos.", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    auth.signInWithEmailAndPassword(sEmail, sPassword)
+                        .addOnCompleteListener(requireActivity()) { task ->
+                            if (task.isSuccessful) {
+                                val activity1: Intent = Intent(requireActivity(), Activity1::class.java)
+                                startActivity(activity1)
+                                requireActivity().finish()
+                            } else {
+                                Snackbar.make(
+                                    requireView(),
+                                    "Inicio de sesión fallido. Revisa tu correo electrónico y contraseña.",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
                 }
             }
+            R.id.btnRegister -> navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+    private fun checkFields(): Boolean {
+        return edtEmail.text.toString().isEmpty() || edtPassword.text.toString().isEmpty()
     }
 }
